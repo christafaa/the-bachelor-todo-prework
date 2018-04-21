cdd7f39c2e1751f1e90c54701e484968b5928c89
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 21:15:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
     if season_name == season
       contestants.each do |contestant|
         if contestant["status"] == "Winner"
-          
+          return contestant["name"].split(' ')[0]
     end
   end
 end
