b3900abbbd12733886492864ff14dc9c1cb3689e
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 21:17:51 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,6 +14,8 @@ def get_contestant_name(data, occupation)
   data.each do |season_name, contestants|
     contestants.each do |contestant|
       return contestant["name"] if contestant["occupation"] == occupation
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
