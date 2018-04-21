f394a3d06b628bc3783019f24ced35cc354b3d96
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 21:21:09 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,7 +29,11 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  
+  data.each do |season_name, contestants|
+    contestants.each do |contestant|
+      
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
